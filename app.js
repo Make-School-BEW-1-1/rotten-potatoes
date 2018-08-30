762d7c1eb30cdb7e00c399fdd57279c2d3d15555
Severiano Badajoz <sevebadajoz@gmail.com>
2018-08-30 12:25:15 -0700

    Add MongoDB support, add Review modle

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-app.get('/', (req, res) => {
-    res.render('home', {msg: 'Hello World!'});
-})
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
@@ -19,6 +18,16 @@ let reviews = [
     { title: "Next Review" }
 ]
 
-app.get('/reviews', (req, res) => {
-    res.render('reviews-index', { reviews: reviews})
+const Review = mongoose.model('Review', {
+    title: String
+});
+
+app.get('/', (req, res) => {
+    Review.find()
+    .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+        console.log(err);
+    })
 })
