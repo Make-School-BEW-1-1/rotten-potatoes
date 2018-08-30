55a47462a34c551b56c3e5a39d45b765105b80c6
Severiano Badajoz <sevebadajoz@gmail.com>
2018-08-30 12:05:22 -0700

    Add ability to see all reviews

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
 
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home', {msg: 'Hello World!'});
 })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
+})
+
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+]
+
+app.get('/reviews', (req, res) => {
+    res.render('reviews-index', { reviews: reviews})
 })
