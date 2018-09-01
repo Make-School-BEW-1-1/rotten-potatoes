824baa013f9806d9b121358309566f157b7531b4
Severiano Badajoz <sevebadajoz@gmail.com>
2018-08-31 17:27:49 -0700

    Allow for user created reviews

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 var exphbs = require('express-handlebars');
 
@@ -13,13 +16,10 @@ app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
 
-let reviews = [
-    { title: "Great Review" },
-    { title: "Next Review" }
-]
-
 const Review = mongoose.model('Review', {
-    title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
 
 app.get('/', (req, res) => {
@@ -31,3 +31,16 @@ app.get('/', (req, res) => {
         console.log(err);
     })
 })
+
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {})
+ })
+
+ app.post('/reviews', (req, res) => {
+     Review.create(req.body).then((review) => {
+         console.log(review);
+         res.redirect('/');
+     }).catch((err) => {
+         console.log(err.message);
+     })
+ })
