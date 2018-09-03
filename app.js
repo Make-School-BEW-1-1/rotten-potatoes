a575e9f3a1a23d7fc490d227e081d0b550d82245
Severiano Badajoz <sevebadajoz@gmail.com>
2018-09-02 18:48:55 -0700

    Remove stupid comma

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const app = express();,
+const app = express();
 const controller = require('./controllers/controller');
 
 app.use(bodyParser.urlencoded({ extended: true }));
